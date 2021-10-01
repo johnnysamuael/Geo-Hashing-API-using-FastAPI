0805aa0bb7908d2d03f31d60b6ba380fbabc85ee
Johny Samuael S <61773527+johnnysamuael@users.noreply.github.com>
2021-10-02 03:03:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,30 +20,3 @@ def encode_latlon(lat: float,lon: float,accuracy: int):
 def encode_latlon(hashval: Text):
     
     return {"coordinates: ": polyline.decode(hashval)}
-
-@app.get("/qr-generate")
-def encode_latlon(lat: float,lon: float,accuracy: int):
-   data = polyline.encode([(lat,lon)],accuracy)
-   qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_H,
-    box_size=10,
-    border=4,)
-
-   qr.add_data(data)
-   qr.make(fit=True)
-   img = qr.make_image(fill_color="black", back_color="white").convert('RGB')
-   img.save("sample.png")
-   #image/png
-   #drive.put('sample.png', img)
-   #hello = drive.get('sample.png')
-   #content = hello.read()
-   #hello.close
-   #content.save("sample.png")
-
-   return FileResponse('sample.png')
-
-
-    
-
-
